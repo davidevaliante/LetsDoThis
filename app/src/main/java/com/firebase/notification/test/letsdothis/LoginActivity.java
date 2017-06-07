d33f4261d57b2994ed6ee5bdbf2497c632afd752
akain <akainurev@gmail.com>
2017-06-07 12:11:17 +0200

    Commit push secong

---
+++ app/src/main/java/com/firebase/notification/test/letsdothis/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.firebase.notification.test.letsdothis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+    private Integer hello = 3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //prova
+    }
+}
